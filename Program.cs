4923bcd89c6db5b3a25fd845fa63efaff98f6b24
Gonzalez Ramirez <70664084+gonzalez26@users.noreply.github.com>
2021-03-23 21:22:36 -0600

    struct

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+struct Books{
+    public string Name, Author, Color;
+    public int Age;
+
+}
+namespace ArrayStruct
+{
+    class Program
+    {
+        static void Main(string[] args)  
+        {   
+
+         Books[] Array = new Books[3];
+            for(int i=0; i<3; i ++){
+                Console.WriteLine("Name: ");
+            Array[i].Name = Console.ReadLine();
+
+            Console.WriteLine("Author: ");
+            Array[i].Author = Console.ReadLine();
+            
+            Console.WriteLine("Color: ");
+            Array[i].Color = Console.ReadLine();
+
+            Console.WriteLine("Age: ");
+            Array[i].Age = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine("Animals: #"+i);
+            }    
+             for(int k=0; k<3; k ++){
+            Console.WriteLine("Name:" + Array[k].Name);
+            Console.WriteLine("Clas:" + Array[k].Author);
+            Console.WriteLine("Country:" + Array[k].Color);
+            Console.WriteLine("Age:" + Array[k].Age+Environment.NewLine);
+
+        Console.WriteLine("Book: #"+k);
+    }
+    }
+    }
+}
